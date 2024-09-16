7361900f76cd19ba32de9ff9b4667a6804c317bb
Johan1020 <londonojohan02@gmail.com>
2024-09-16 16:12:28 -0500

    dsds

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      jquery: "jquery/dist/jquery.min.js",
+    },
+  },
+  optimizeDeps: {
+    include: ["jquery"], // Asegúrate de que Vite optimice jQuery
+  },
+});
